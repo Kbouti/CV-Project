7256d321aabf1ae897de0b1012a4d887082df117
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-25 14:30:08 -0700

    First project commit

---
+++ src/components/testComponent.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function TestComponent() {
+  return (
+    <>
+      <h1>Just out fer a rip, are ya bud? </h1>
+      <h2>Just out fer a rip?</h2>
+    </>
+  );
+}
+
+
+export {TestComponent}
