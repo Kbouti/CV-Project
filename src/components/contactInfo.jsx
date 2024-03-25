b1276f6d031b1470a385bce8b1b168eab3497386
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-25 16:16:29 -0700

    Create contactinfo form inputs

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 
 const ContactInfo = () => {
-return (
-    <div className="contactInfo">
-        <h1 className="sectionHeader">General Information</h1>
-        
-    </div>
-)
-}
+  return (
+    <div className="contactInfo section">
+      <h1 className="sectionHeader">General Information</h1>
 
-export {ContactInfo};
+      <div className="formEntry">
+        <label htmlFor="name">Full Name</label>
+        <input name="name" type="text"></input>
+      </div>
+
+      <div className="formEntry">
+        <label htmlFor="email">Email</label>
+        <input name="email" type="email"></input>
+      </div>
+
+      <div className="formEntry">
+        <label htmlFor="phone">Phone</label>
+        <input name="phone" type="phone"></input>
+      </div>
+
+      <div className="formEntry">
+        <label htmlFor="city">City</label>
+        <input name="city" type="text"></input>
+      </div>
+    </div>
+  );
+};
+
+export { ContactInfo };
