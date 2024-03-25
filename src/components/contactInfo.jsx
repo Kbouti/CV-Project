8adcbfcf14226844fb21fd40a9215e5a91bbfed7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-25 15:42:47 -0700

    Create contactInfo section

---
+++ src/components/contactInfo.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const ContactInfo = () => {
+return (
+    <div className="contactInfo">
+        <h1 className="sectionHeader">General Information</h1>
+        
+    </div>
+)
+}
+
+export {ContactInfo};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
-import { TestComponent } from './components/testComponent.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+import { TestComponent } from './components/testComponent.jsx';
+import { ContactInfo } from './components/contactInfo.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TestComponent />
+    <ContactInfo />
   </React.StrictMode>,
 )
