adfd939ddcde4e9a5c1b5c9c78fa95b7749606f2
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-25 21:37:17 -0700

    Resize resume to printer page proportions

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-
 const MainOutput = () => {
-    return (
-        <div className="resume mainSection">
-            <h2>Resume</h2>
-        </div>
-    )
-}
+  return (
+    <div className="resume mainSection">
+      <div className="resumePage">
+        <h2>Resume</h2>
+      </div>
+    </div>
+  );
+};
 
-export {MainOutput}
+export { MainOutput };
