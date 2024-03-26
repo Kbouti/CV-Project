82af511c9cc6a06a61a4df7392946a97a2f3117e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-25 17:07:48 -0700

    Render all section components with borders

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 
-import { TestComponent } from "./components/testComponent.jsx";
 import { ContactInfo } from "./components/contactInfo.jsx";
 import { Education } from "./components/education.jsx";
 import { Experience } from "./components/experience.jsx";
+import { MainOutput } from "./components/output.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TestComponent />
+    <MainOutput />
     <ContactInfo />
     <Education />
     <Experience />
