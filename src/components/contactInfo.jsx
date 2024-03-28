d591984b36f675abf1a7ce872ccd21b7372fef7c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-27 18:42:37 -0700

    Create form element for general information

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -1,61 +1,93 @@
 import { useState } from "react";
 
-const ContactInfo = ({fullName, fullNameSetter, email, emailSetter, phone, phoneSetter, city, citySetter}) => {
+const ContactInfo = ({
+  fullName,
+  fullNameSetter,
+  email,
+  emailSetter,
+  phone,
+  phoneSetter,
+  city,
+  citySetter,
+}) => {
+  function submitChanges() {
+    // Ultimately this should be the onClick callback. This function will get the values of the inputs.
+    // We'll have the option of replacing placeholder text with new text or leaving placeholder text if there is nouser input value
+    //  getValues()
+    //  setValues(values)
+  }
 
+  function getValues() {
+    const newName = document.getElementById("nameInput").value;
+    const newPhone = document.getElementById("phoneInput").value;
+    const newEmail = document.getElementById("emailInput").value;
+    const newCity = document.getElementById("cityInput").value;
 
+    {
+      fullNameSetter(newName);
+    }
+    {
+      phoneSetter(newPhone);
+    }
+    {
+      emailSetter(newEmail);
+    }
+    {
+      citySetter(newCity);
+    }
+  }
 
-function submitChanges(){
-// Ultimately this should be the onClick callback. This function will get the values of the inputs. 
-// We'll have the option of replacing placeholder text with new text or leaving placeholder text if there is nouser input value
-//  getValues()
-//  setValues(values)
-
-}
-
-function getValues(){
-  const newName = document.getElementById("nameInput").value;
-  const newPhone = document.getElementById("phoneInput").value;
-  const newEmail = document.getElementById("emailInput").value;
-  const newCity = document.getElementById("cityInput").value;
-
-  {fullNameSetter(newName)}
-  {phoneSetter(newPhone)}
-  {emailSetter(newEmail)}
-  {citySetter(newCity)}
-}
-
-function setValues(newValues){
-  // we'll update values, but leave the placeholder if the rest is blank? Maybe?
-}
-
-
-
+  function setValues(newValues) {
+    // we'll update values, but leave the placeholder if the rest is blank? Maybe?
+  }
 
   return (
     <div className="contactInfo subSection">
       <h1 className="sectionHeader">General Information</h1>
 
-      <div className="formEntry">
-        <label htmlFor="name">Full Name</label>
-        <input id="nameInput" name="name" type="text" placeholder="Derek Zoolander"></input>
-      </div>
+      <form>
+        <div className="formEntry">
+          <label htmlFor="name">Full Name</label>
+          <input
+            id="nameInput"
+            name="name"
+            type="text"
+            placeholder="Derek Zoolander"
+          ></input>
+        </div>
 
-      <div className="formEntry">
-        <label htmlFor="email">Email</label>
-        <input id="emailInput" name="email" type="email" placeholder="DZoo@Ballzmodels.com"></input>
-      </div>
+        <div className="formEntry">
+          <label htmlFor="email">Email</label>
+          <input
+            id="emailInput"
+            name="email"
+            type="email"
+            placeholder="DZoo@Ballzmodels.com"
+          ></input>
+        </div>
 
-      <div className="formEntry">
-        <label htmlFor="phone">Phone</label>
-        <input id="phoneInput"name="phone" type="phone" placeholder="212-555-5378"></input>
-      </div>
+        <div className="formEntry">
+          <label htmlFor="phone">Phone</label>
+          <input
+            id="phoneInput"
+            name="phone"
+            type="phone"
+            placeholder="212-555-5378"
+          ></input>
+        </div>
 
-      <div className="formEntry">
-        <label htmlFor="city">City</label>
-        <input id="cityInput" name="city" type="text" placeholder="New York"></input>
-      </div>
+        <div className="formEntry">
+          <label htmlFor="city">City</label>
+          <input
+            id="cityInput"
+            name="city"
+            type="text"
+            placeholder="New York"
+          ></input>
+        </div>
 
- <button onClick={getValues}>Submit Changes</button>
+        <button onClick={getValues}>Submit Changes</button>
+      </form>
     </div>
   );
 };
