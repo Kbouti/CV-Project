ee82520874fa99f352e33bbefb7ee643bf4d291e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-08 11:07:10 -0700

    Add class to non-bottom education entries

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -10,6 +10,17 @@ const MainOutput = ({
   educationObjects,
   experienceObjects,
 }) => {
+
+// ********************************************************************************************
+// This adds the class purple to every education object except the last one. Alter the purple class in CSS to instead create some kind of underline or barrier between entries
+  const educationEntries = document.getElementsByClassName("educationEntry");
+  for (let i = 0; i < educationEntries.length; i++) {
+    if (educationEntries[i] !== undefined) {
+      educationEntries[i].classList.add("purple");
+    }
+  }
+// ********************************************************************************************
+
   return (
     <div className="resume mainSection">
       <div className="resumePage">
@@ -41,10 +52,9 @@ const MainOutput = ({
               <div key={object.key} className="experienceEntry userEntry">
                 <p>{object.years}</p>
                 <h4>{object.organization}</h4>
-                <p>{object.location}</p> 
+                <p>{object.location}</p>
                 <p>{object.position}</p>
                 <p className="jobDescription">{object.description}</p>
-
               </div>
             );
           })}
